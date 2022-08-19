83f7012bd69df013fe885e9a4c569307cbf4defb
pmohanj <pmohan6712@gmail.com>
2022-08-19 23:19:38 +0530

    Frontend and backend developed

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.css"
+import {Container} from "semantic-ui-react"
+import ToDoList from "./To-Do-List"
+
+export default function App(){
+    return(
+        <div>
+            <Container>
+                <ToDoList/>
+            </Container>
+        </div>
+    )
+}
